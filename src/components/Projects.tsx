6867e7a4e6b8b42e5e22de5ae7aefca31dbcea12
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2025-03-11 14:04:14 -0400

    Update Projects.tsx swapped images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ const projectsData = [
     title: "IRS Free File",
     description: "Enhancing the user experience to improve confidence and understanding when deciding on tax prep software.",
     category: "UX Design | Web App",
-    image: "/lovable-uploads/free-file-2.png",
+    image: "/lovable-uploads/free-file-0.png",
     slug: "free-file"
   },
   {
